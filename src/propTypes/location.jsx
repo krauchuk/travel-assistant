3f72d79e5816c11ef6b24b387e9e3980385e8b30
krauchuk <ihar.krauchuk@gmail.com>
2019-11-16 18:39:09 +0300

    'propTypes' folder has been deleted

--- src/propTypes/location.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-export default PropTypes.shape({
-  hash: PropTypes.string,
-  key: PropTypes.string,
-  pathname: PropTypes.string,
-  search: PropTypes.string,
-  state: PropTypes.string,
-});
